c548e8af2a24e89731883ef9bbb89c1b5b7b5134
dhanish <dhanish.b.gajjar@gmail.com>
2017-05-01 14:40:50 +0100

    Files Updated and Patched

--- app.js
+++ app.js
@@ -3,14 +3,16 @@
 // Text field, where the task is typed.
 const taskInput = document.querySelector('input.task');
 
-// Parent Element of the list items
 const tasks = document.querySelector('ul.tasks');
-
-// All list items
 const list = tasks.children;
-const deleteB = document.querySelector('svg.delete');
+const imageSrc = "delete.svg";
 
-
+function attachButton(li) {
+  let deleteButton = document.createElement('img');
+  deleteButton.className = 'delete';
+  deleteButton.setAttribute("src", imageSrc);
+  li.appendChild(deleteButton);
+}
 
 for (let i = 0; i < list.length; i += 1) {
   attachButton(list[i]);
@@ -20,30 +22,17 @@ taskInput.addEventListener('keyup', (e) => {
   if (e.keyCode == 13) {
 		let ul = document.getElementsByTagName('ul')[0];
 	  let li = document.createElement('li');
-
 	  li.textContent = task.value;
-    attachButton(li);
+		attachButton(li);
 	  ul.appendChild(li);
 	  task.value = '';
 	}
 });
 
-// $(".bin").on('click', function(){
-//   alert("Hey");
-// });
-
 tasks.addEventListener('click', (event) => {
-  if (event.target.tagName == 'svg') {
+  if (event.target.tagName == 'IMG') {
     if (event.target.className == 'delete') {
-      let li = event.target.parentNode.parentNode;
-      let ul = li.parentNode;
-      ul.removeChild(li);
-    } else if (event.target.className == 'bin') {
-      let li = event.target.parentNode.parentNode.parentNode;
-      let ul = li.parentNode;
-      ul.removeChild(li);
-    } else if ((event.target.className == 'cap') || (event.target.className == 'can')) {
-      let li = event.target.parentNode.parentNode.parentNode.parentNode;
+      let li = event.target.parentNode;
       let ul = li.parentNode;
       ul.removeChild(li);
     }
